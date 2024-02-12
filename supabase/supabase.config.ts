0158782f4ab0b4ea6e0149a702698405b28d908b
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-02-12 21:57:05 +0900

    setting: supabase 연결

---
+++ supabase/supabase.config.ts
@@ -0,0 +1,10 @@
+import {createClient} from '@supabase/supabase-js';
+
+import type { Database } from '@/types/supabase';
+
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient<Database>(supabaseUrl, supabaseKey, {auth: {autoRefreshToken: true}});
+
+export {supabase};
