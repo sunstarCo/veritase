a1a6377300a9c9822bd14bb0b9bedb1257ad5244
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-02-14 23:05:05 +0900

    fix:이미지 변경

--- src/app/education/teacher/page.tsx
+++ src/app/education/teacher/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div>
       <Breadcrumb title="수능강사" sub_text="최고의 강사진으로 구성된 베리타스 교육" />
-      <div className="max-w-[1280px] mx-auto px-12 md:px-36 py-24">
+      <div className="max-w-[1280px] mx-auto px-6 md:px-36 py-24">
         <section className="flex flex-col md:flex-row justify-center gap-16">
           <div className="w-full">
             <Image src={'/teacher/수능강사copywrite.svg'} alt="" width={0} height={0} sizes="100" className="w-full" />
@@ -34,8 +34,15 @@ export default function Page() {
             sizes="100"
             className="w-full md:hidden"
           />
-          <div className="absolute top-[5%] left-1/2 -translate-x-1/3 w-1/2">
-            <Image src={'/teacher/강사사진.png'} alt="" width={0} height={0} sizes="100" className="w-3/4 md:hidden" />
+          <div className="absolute top-[7%] left-1/2 -translate-x-1/3 w-1/2">
+            <Image
+              src={'/teacher/강사사진.png'}
+              alt="강사사진"
+              width={0}
+              height={0}
+              sizes="100"
+              className="w-2/3 md:hidden"
+            />
           </div>
         </div>
       </div>
