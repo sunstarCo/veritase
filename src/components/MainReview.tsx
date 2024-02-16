fb98b5f70b0660ddc3cbd6f6bc846d2b59ec8b84
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-02-16 15:56:20 +0900

    fix:디자인 수정

--- src/components/MainReview.tsx
+++ src/components/MainReview.tsx
@@ -8,8 +8,8 @@ import Slider from './common/Slider';
 function MainReview() {
   return (
     <div className="w-full md:max-w-96 lg:w-72 mx-auto flex flex-col gap-4">
-      <div className="p-6 text-white bg-blue-5 rounded w-full">
-        <p className="text-lg font-bold text-center">(주)베리타스교육 첫수업 반응</p>
+      <div className="p-4 text-white bg-blue-5 rounded w-full">
+        <p className="text-xl sm:text-lg font-bold text-center">(주)베리타스교육 첫수업 반응</p>
         <div className="mt-4 flex flex-col gap-4">
           <Slider type="main" />
         </div>
