008b062a3111f2f719ba7bce4a39d273480fc979
hanbyul-choi <hanbyul1025@gmail.com>
2024-02-28 14:38:47 +0900

    feat:메인 텍스트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,11 @@ export default function Home() {
           alt="메인배너"
           priority
         />
-        <div className="flex flex-col min-[410px]:flex-row absolute top-[15%] sm:top-[30%] break-keep w-full px-[10%]">
+        <div className="flex flex-col min-[440px]:flex-row absolute top-[15%] sm:top-[30%] break-keep w-full px-[10%]">
           <h1 className="text-white text-nowrap text-[3rem] lg:text-[4.5rem] font-semibold transition-transform animate-show opacity-0 px-1">
             수능단기 고득점,&nbsp;
           </h1>
-          <h1 className="text-white text-[3rem] lg:text-[4.5rem] max-[410px]:ml-auto font-semibold transition-transform animate-show_delay_1s opacity-0">
+          <h1 className="text-white text-[3rem] lg:text-[4.5rem] max-[440px]:ml-auto font-semibold transition-transform animate-show_delay_1s opacity-0">
             가능하다!
           </h1>
         </div>
