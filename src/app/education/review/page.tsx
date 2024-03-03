cefae2e8ee6403474db88ca80437074e0645d663
rimsunwoo <dlatjsdn205@naver.com>
2024-03-03 14:17:13 +0900

    수강후기 수정

--- src/app/education/review/page.tsx
+++ src/app/education/review/page.tsx
@@ -8,15 +8,25 @@ import BannerLayout from '@/components/common/BannerLayout';
 
 export default function Page() {
   const [curImg, setCurImg] = useState(1);
+  const [oddImg, setOddImg] = useState(1);
+  const [evenImg, setEvenImg] = useState(2);
+
+  const isOdd = curImg % 2 === 1;
+
   const onClickHandler = () => {
     if (curImg === 77) {
       setCurImg(1);
-    } else {
-      setCurImg(prev => prev + 1);
+      setOddImg(1);
+      setEvenImg(2);
+      return;
     }
+    setCurImg(prev => prev + 1);
+    if (isOdd) {
+      setOddImg(prev => prev + 2);
+      return;
+    }
+    setEvenImg(prev => prev + 2);
   };
-
-  // const isOdd = curImg % 2 === 1;
 
   return (
     <BannerLayout className="flex justify-center items-start sm:items-center min-h-[600px]">
@@ -29,13 +39,13 @@ export default function Page() {
         </div>
         <div
           onClick={onClickHandler}
-          className="cursor-pointer relative bg-[url('/reviews/모바일사진.svg')] p-3 bg-cover w-[240px] h-[430px] md:h-[550px] md:w-[310px] flex items-center">
+          className="cursor-pointer relative  bg-[url('/reviews/모바일사진.svg')] p-3 bg-cover w-[240px] h-[430px] md:h-[550px] md:w-[310px] flex items-center">
           <Image
             src={`https://onuqprohgtghlcbucalq.supabase.co/storage/v1/object/public/reviews/${oddImg}.jpg`}
             alt="후기메세지"
             width={300}
             height={500}
-            className={`w-[220px] md:w-[285px]  ${!isOdd && 'hidden'}`}
+            className={`w-[220px] md:w-[285px]   ${!isOdd && 'hidden'}`}
             placeholder="blur"
             blurDataURL={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==`}
           />
