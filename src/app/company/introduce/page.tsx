9e869d99a25294d78047d3cebb05421aa0194a37
rimsunwoo <dlatjsdn205@naver.com>
2024-03-03 12:39:34 +0900

    수능강사, 회사소개 수정사항 반영

--- src/app/company/introduce/page.tsx
+++ src/app/company/introduce/page.tsx
@@ -25,26 +25,24 @@ export default function Page() {
           <div className="w-full bg-blue-4 text-2xl md:text-3xl font-semibold text-center text-white py-6 px-9">
             <p>신뢰와 믿음 / 혁신적 마인드로 학생의 변화를 선도하고 있습니다.</p>
           </div>
-          <div className="flex gap-10 items-start mt-16">
-            <div className="w-[7px] h-[70px] mt-1 bg-blue-4" />
+          <div className=" mt-16 pl-10">
             <div>
-              <div className="text-2xl md:text-3xl flex flex-col justify-center gap-2 font-semibold">
+              <div className="text-2xl md:text-3xl flex flex-col justify-center gap-2  font-semibold">
                 <p>베리타스 교육은 강남 최고 강사들이</p>
                 <p>1:1로 수준높은 강의를 제공하는 입시 전문회사 입니다.</p>
               </div>
-
-              <p className="w-full break-keep mt-10 leading-[30px] font-medium pr-8">
+              <p className="w-full break-keep sm:border-l-4 border-l-2 border-blue-4 pl-5 mt-10 leading-[30px] font-medium pr-8">
                 내신 성적이 부족해 수능에 올인하겠다는 학생 / 모의평가 성적이 내신성적에 비해 턱없이 부족한 학생 / 수능
-                실패를 경험한 재수생/ 현재 자신의 위치보다 최상의 점수를 올리고자 하는 학생들에게 특화된 핵심 강의를
-                최고의 강사진들을 통해 생동감 넘치는 강의를 제공하는 회사입니다. <br />
+                실패를 경험한 재수생/ 현재 자신의 위치보다 최상의 점수를 올리고자 하는 학생들에게 <br /> 특화된 핵심
+                강의를 최고의 강사진들을 통해 생동감 넘치는 강의를 제공하는 회사입니다. <br />
               </p>
             </div>
           </div>
-          <div className="flex gap-10 items-start mt-10">
-            <div className="w-[12px] h-[66px] bg-blue-4" />
+          <div className="mt-10 pl-10">
             <div>
               <p className="text-2xl md:text-3xl font-semibold py-4">강남 최고의 강사진 보유</p>
-              <p className="w-full break-keep mt-10 leading-[30px] font-medium pr-8">
+
+              <p className="w-full break-keep sm:border-l-4 border-l-2 border-blue-4  pl-5 mt-10 leading-[30px] font-medium pr-8">
                 (주)베리타스교육은 서울 강남 지역에서 최고로 꼽히는 강사진을 자랑하며, 학생들의 고득점을 이루어낸 검증된
                 강사들로 구성되어 있습니다. 이들은 수능문제의 <span className="text-blue-4 font-bold">출제 의도</span>를
                 꿰뚫고, 다양한 풀이의 접근 방식을 제공하며, <span className="text-blue-4 font-bold">확인학습</span>을
--- src/app/education/teacher/page.tsx
+++ src/app/education/teacher/page.tsx
@@ -16,10 +16,13 @@ export default function Page() {
   return (
     <div>
       <Breadcrumb title="수능강사" sub_text="최고의 강사진으로 구성된 베리타스 교육" />
-      <div className="max-w-[1700px] mx-auto px-6 md:px-12 py-12">
+      <div className="max-w-[1700px] mx-auto px-6 md:px-12 pt-12 sm:pb-11">
         {arr.map(item => (
           <TeacherBox num={item.id} position={item.position} key={item.id} />
         ))}
+      </div>
+      <div className="w-full bg-blue-4 text-2xl md:text-3xl font-semibold text-center break-keep text-white py-6 px-9">
+        <p>타사에서 강사 정보 유출사례가 빈번하여, 당사는 모든 강사진들의 정보를 홈페이지에 공개하지 않습니다.</p>
       </div>
     </div>
   );
