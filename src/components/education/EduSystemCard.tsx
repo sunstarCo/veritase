0d3abadfbe307ae261eb2228ee4dbec68315ab2c
hanbyul-choi <hanbyul1025@gmail.com>
2024-03-05 16:31:12 +0900

    design: 행간 수정

--- src/components/education/EduSystemCard.tsx
+++ src/components/education/EduSystemCard.tsx
@@ -12,19 +12,19 @@ function EduSystemCard({card}: {card: any}) {
           </p>
         </div>
         <div className="w-full card-back">
-          <div className="bg-blue-4 rounded-t-md py-8 md:py-12 px-4 md:px-4 flex gap-2 justify-center items-center text-white min-h-24 min-[870px]:min-h-48">
+          <div className="bg-blue-4 rounded-t-md py-8 md:py-12 px-4 md:px-4 flex gap-2 justify-center items-center text-white h-36 min-[870px]:min-h-40">
             <p className="text-2xl md:text-3xl font-medium text-nowrap">{card.title} / </p>
             <p className="text-lg md:text-xl font-medium mt-2">
               {card.times[0]}회 ~ {card.times[1]}회
             </p>
           </div>
-          <div className="bg-[#e7e7e7] rounded-b-md pt-20 md:pt-44 px-4 h-[280px] md:h-[450px]">
+          <div className="bg-[#e7e7e7] rounded-b-md pt-20 md:pt-44 px-4 pb-16 flex flex-col justify-end h-[340px] md:h-[420px]">
             {card.desc.map((text: any, i: number) => (
-              <div key={i} className="px-2 md:min-h-28">
+              <div key={i} className="px-2">
                 <p className="text-2xl font-semibold text-blue-4">{text.title}</p>
-                <div className="px-5 py-2 text-lg font-medium">
+                <div className="px-5 text-xl font-medium">
                   {text.content.map((text: any, i: number) => (
-                    <p key={i} className="list-item">
+                    <p key={i} className="list-item text-lg">
                       {text}
                     </p>
                   ))}
