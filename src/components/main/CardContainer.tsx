55d8453cc9ad61c062ae0d540d8be361f966b927
rimsunwoo <dlatjsdn205@naver.com>
2024-03-05 15:32:45 +0900

    메인 카드 애니메이션 속도 변경

--- src/components/main/CardContainer.tsx
+++ src/components/main/CardContainer.tsx
@@ -37,7 +37,7 @@ function CardContainer() {
         <Link
           key={i}
           href={card.path}
-          className={`${card.img} card bg-no-repeat min-h-[400px] md:min-h-[500px] xl:min-h-[600px] flex items-end min-w-[210px] md:min-w-[260px] md:w-1/4 hover:w-[50%] break-keep transition-all duration-400 ease-out rounded-lg overflow-hidden`}>
+          className={`${card.img} card bg-no-repeat min-h-[400px] md:min-h-[500px] xl:min-h-[600px] flex items-end min-w-[210px] md:min-w-[260px] md:w-1/4 hover:w-[50%] break-keep transition-all duration-500 ease-out rounded-lg overflow-hidden`}>
           <div className="article text-white from-[rgba(0,0,0,0.4)] to-transparent w-full px-8 pb-8 pt-8 min-h-[180px] flex flex-col bg-gradient-to-t gap-3 justify-end">
             <h3 className="text-2xl lg:text-4xl font-bold">{card.title}</h3>
             <p className="overflow-hidden overflow-ellipsis text-nowrap">{card.text}</p>
